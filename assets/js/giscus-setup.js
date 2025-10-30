7f6d1f179925d65d7142ee83bb8ec6f30342bf57
valentinburiasco <valentin.buriasco@gmail.com>
2025-10-30 15:11:40 +0000

    Deploying to gh-pages from @ valentinburiasco/valentinburiasco.github.io@938887cc7b412549fcfd80c9ecde90990836cc52 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "valentinburiasco/vburiasco.git.io",
+    "data-repo": "valentinburiasco/valentinburiasco.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
